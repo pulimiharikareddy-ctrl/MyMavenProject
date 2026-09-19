ee6a8e165eacc3de62b0f6dc93f2e496dcd09352
pulimi <pulimiharikareddy@gmail.com>
2026-09-19 12:10:18 +0530

    Updated Project

---
+++ src/main/java/com/portfolio/App.java
@@ -0,0 +1,15 @@
+package com.portfolio;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("======================");
+        System.out.println("welcome to portfolioapp");		
+        System.out.println("========================");	
+    
+    }
+       
+   
+}   
+    
+   
+    
+
